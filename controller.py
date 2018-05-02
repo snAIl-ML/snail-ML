04bfcbce734d94533be3bc2553a74fb9cd66d761
Vivian Allen <mrvivianallen@gmail.com>
2018-05-02 16:57:07 +0100

    controller#take extra photo function removed, calls  to #create_function removed from movement functions

--- controller.py
+++ controller.py
@@ -11,40 +11,29 @@ class Controller(object):
         self.window.title('snAIl Controller')
         self.window.bgcolor('blue')
 
-    def create_photo(self):
+    def create_temp_photo(self):
         img_data = camera.grab_image_data()
         dir_path = camera.create_path("temp")
         camera.create_directory(dir_path)
-        camera.save_photo(dir_path, img_data)
-        return dir_path
+        return camera.save_photo(dir_path, img_data)
 
     def up(self):
-        self.create_photo('forward')
         self.car.forward(0.2)
 
     def down(self):
-        self.create_photo('reverse')
         self.car.reverse(0.2)
 
     def right(self):
-        self.create_photo('turn_right')
         self.car.turn_right(0.2)
 
     def left(self):
-        self.create_photo('turn left')
         self.car.turn_left(0.2)
 
     def piv_right(self):
-        self.create_photo('pivot_right')
         self.car.pivot_right(0.2)
 
     def piv_left(self):
-        self.create_photo('pivot_left')
         self.car.pivot_left(0.2)
-
-    def take_extra_photo(self):
-        print('Taking extra photo')
-        self.create_photo('extra')
 
     def exit(self):
         self.window.bye()
@@ -57,7 +46,6 @@ def main():
     w.window.onkey(w.left, 'Left')
     w.window.onkey(w.piv_right, '/')
     w.window.onkey(w.piv_left, '.')
-    w.window.onkey(w.take_extra_photo, 'p')
     w.window.onkey(w.exit, 'q')
     w.window.listen()
     w.window.mainloop()
