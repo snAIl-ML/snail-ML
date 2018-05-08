19fc94feda250f909e52d183e7163b62a76c2838
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-05-08 01:39:02 +0100

    Fix 3/4 code violations

--- get_move_from_server.py
+++ get_move_from_server.py
@@ -1,7 +1,8 @@
-'Module to ger response from the server'
+'Module to get response from the server'
 import os
 
 def get_server_move(image_path, url):
+    'Function to get response from the server'
     #nb this is acknowledged as terrible, terrible code
     response_file = "response.txt"
     post_command = ("curl -o response.txt -F 'image=@" +
