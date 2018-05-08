8a8d6c6199bd79545d0edd8afccff614787ac37b
Vivian Allen <mrvivianallen@gmail.com>
2018-05-08 14:26:20 +0100

    remoteAPI forward route, tests passing

--- remoteAPI.py
+++ remoteAPI.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from controller import Controller
 
 app = Flask(__name__)
+
+controller = Controller()
 
 @app.route("/")
 def index():
     return 'snail remote control API'
+
+@app.route("/forward")
+def forward():
+    controller.up()
+    return 'forward'
 
 if __name__ == "__main__":
     app.run()
--- test/test_remoteAPI.py
+++ test/test_remoteAPI.py
@@ -1,8 +1,17 @@
 import path_helper_test_main
-from remoteAPI import app
+from remoteAPI import *
 
-def test_index_page():
+
+def test_index_route():
     tester = app.test_client()
     response = tester.get('/', content_type='html/text')
     assert (response.status_code) == 200
     assert ('snail remote control API' in response.data.decode("utf8"))
+
+def test_forward_route(mocker):
+    tester = app.test_client()
+    mocker.patch.object(controller, 'up')
+    response = tester.get('/forward', content_type='html/test')
+    assert (response.status_code) == 200
+    assert ('forward' in response.data.decode("utf8"))
+    assert (controller.up.called)
