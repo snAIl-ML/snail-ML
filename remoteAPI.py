fb49734a205c7c0f0f85c7c620be2041c74ea6c4
Vivian Allen <mrvivianallen@gmail.com>
2018-05-08 14:34:21 +0100

    index page calls create_temp_photo, piv_left route calls piv_left, tests passing

--- remoteAPI.py
+++ remoteAPI.py
@@ -7,12 +7,18 @@ controller = Controller()
 
 @app.route("/")
 def index():
+    controller.create_temp_photo()
     return 'snail remote control API'
 
 @app.route("/forward")
 def forward():
     controller.up()
     return 'forward'
+
+@app.route("/piv_left")
+def piv_left():
+    controller.piv_left()
+    return "piv_left"
 
 if __name__ == "__main__":
     app.run()
--- test/test_remoteAPI.py
+++ test/test_remoteAPI.py
@@ -2,13 +2,15 @@ import path_helper_test_main
 from remoteAPI import *
 
 
-def test_index_route():
+def test_index_route_initializes_photo_loop(mocker):
     tester = app.test_client()
+    mocker.patch.object(controller, 'create_temp_photo')
     response = tester.get('/', content_type='html/text')
     assert (response.status_code) == 200
     assert ('snail remote control API' in response.data.decode("utf8"))
+    assert (controller.create_temp_photo.called)
 
-def test_forward_route(mocker):
+def test_forward_route_calls_up_function(mocker):
     tester = app.test_client()
     mocker.patch.object(controller, 'up')
     response = tester.get('/forward', content_type='html/test')
@@ -16,7 +18,7 @@ def test_forward_route(mocker):
     assert ('forward' in response.data.decode("utf8"))
     assert (controller.up.called)
 
-def test_pivot_left_route(mocker):
+def test_pivot_left_route_calls_pivot_left_function(mocker):
     tester = app.test_client()
     mocker.patch.object(controller, 'piv_left')
     response = tester.get('/piv_left', content_type='html/test')
