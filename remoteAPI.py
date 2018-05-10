a6632a8c39212b9a7f376c08f737e9d673cd0cfc
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-05-10 07:38:10 +0100

    Fix 6/11 pylint conflicts in remoteAPI.py

--- remoteAPI.py
+++ remoteAPI.py
@@ -1,3 +1,4 @@
+'Remote API Module'
 from flask import Flask, render_template, url_for, redirect
 from controller import Controller
 
@@ -7,21 +8,25 @@ controller = Controller()
 
 @app.route("/")
 def index():
+    'Function for main route'
     controller.create_temp_photo()
-    return render_template ('index.html', image_path = controller.get_photoname())
+    return render_template('index.html', image_path = controller.get_photoname())
 
 @app.route("/forward")
 def forward():
+    'Function for forward command'
     controller.up()
     return redirect('/')
 
 @app.route("/piv_left")
 def piv_left():
+    'Function for left command'
     controller.piv_left()
     return redirect('/')
 
 @app.route("/piv_right")
 def piv_right():
+    'Function for right command'
     controller.piv_right()
     return redirect('/')
 
